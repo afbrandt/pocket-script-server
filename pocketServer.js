d626710a6482b0cb8df6482a62da9f9482305349
Andrew Brandt <Andrew@Xerxes-5.local>
2014-11-06 15:18:32 -0800

    start reading form fields

--- pocketServer.js
+++ pocketServer.js
@@ -19,14 +19,24 @@ app.get('/', function(req, res) {
 
 app.get('/objects/', function(req, res) {
   s3.listObjects({Bucket: 'pocketserver-development'}, function(er, data) {
-    if (er) {}
-    res.send(data)
+    if (er) {
+      res.send(400, er)
+    } else {
+      res.send(200, data)
+    }
   })
 })
 
 app.post('/object/', function(req, res) {
   console.log('processing POST')
-  req.pipe(req.busboy)
+  var orderLabel
+  
+  var error = null;
+  req.busboy.on('field', function(fieldName, val, fieldNameTruncated, valTruncated) {
+    if (fieldName === 'deviceToken') {
+      console.log('field: ' + fieldName + ' value: ' + val)
+    }
+  })
   req.busboy.on('file', function(fieldName, file, fileName) {
     //console.log(file.length)
     
@@ -45,7 +55,9 @@ app.post('/object/', function(req, res) {
        s3.putObject({Bucket: 'pocketserver-development', Body: finalBuffer, Key: fileName, ContentLength: finalBuffer.length}, function(err, data) {
         if (err) {
           console.log(err)
-          res.send(400)
+          error = err
+          //didCompleteSuccess = false
+          //res.send(400)
         }
         else {
           //res.send(201)
@@ -54,7 +66,15 @@ app.post('/object/', function(req, res) {
       })
     })
   })
-  res.send(201)
+  req.pipe(req.busboy)
+  req.busboy.on('end', function() {
+  	if (error) {
+  	  res.send(400, error)
+  	}
+  	else {
+  	  res.send(201)
+  	}
+  })
 })
 
 app.listen(3000)
